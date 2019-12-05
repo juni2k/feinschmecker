787dfb3ee9665e473e038016ee3500d30ec1c2a5
Martin Borchert <martin-anon@posteo.de>
2019-12-05 19:49:04 +0100

    Make commands return *commands.TextMap instead of *reply.Reply

--- commands/about.go
+++ commands/about.go
@@ -4,17 +4,16 @@ import (
 	"github.com/nanont/feinschmecker/config"
 	"github.com/nanont/feinschmecker/filter"
 	"github.com/nanont/feinschmecker/lang"
-	"github.com/nanont/feinschmecker/reply"
 	"github.com/nanont/feinschmecker/sessions"
 )
 
 // about.go contains the about command.
 
-func About(conf *config.Config, session *sessions.Session) *reply.Reply {
-	return &reply.Reply{TextMap: map[lang.Language]string{
+func About(conf *config.Config, session *sessions.Session) *TextMap {
+	return &TextMap{
 		lang.En: filter.AddHeading("Key",
 			filter.Perl("", "./icons.pl", "en")),
 		lang.De: filter.AddHeading("Legende",
 			filter.Perl("", "./icons.pl", "de")),
-	}}
+	}
 }
